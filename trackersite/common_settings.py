a7de3994465d6755a0ff1633bc63e0aa06c4833c
Petr Novak <petr.novak@cosmoboy.cz>
2011-06-19 19:27:00 +0200

    user creation with a message

--- trackersite/common_settings.py
+++ trackersite/common_settings.py
@@ -2,8 +2,25 @@
 from os.path import dirname, abspath, join
 PROJECT_DIR = abspath(join(dirname(__file__), '..'))
 
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
 TEMPLATE_DIRS = (
     join(PROJECT_DIR, 'templates'),
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages"
 )
 
 STATICFILES_DIRS = (
@@ -18,6 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.staticfiles',
     'django.contrib.comments',
+    'django.contrib.messages',
     'south',
     'tracker',
     'users',
@@ -25,3 +43,4 @@ INSTALLED_APPS = (
 
 ROOT_URLCONF = 'urls'
 LOGIN_REDIRECT_URL = '/'
+MESSAGE_STORAGE = 'django.contrib.messages.storage.fallback.FallbackStorage'
--- trackersite/users/views.py
+++ trackersite/users/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/register.html'
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'User %s created.' % form.cleaned_data['username'])
+        return HttpResponseRedirect(reverse('ticket_list'))
+register = RegisterView.as_view()
