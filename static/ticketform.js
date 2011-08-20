3f16a137c5a4aaccda2b66e6c605a050dcf0c661
Petr Novak <petr.novak@cosmoboy.cz>
2011-08-20 21:09:30 +0200

    fix media form showing

--- static/ticketform.js
+++ static/ticketform.js
@@ -10,7 +10,7 @@ function refresh_description()
 
 	var topic = topics_table[topic_id];
 	$('#topic_desc').html(topic['form_description']).toggle(topic['form_description'] != "");
-	$('#mediainfo-group').toggle(topic['detailed_tickets']);
+	$('#mediainfo-group').toggle(topic['ticket_media']);
 }
 
 $(document).ready(function() {
