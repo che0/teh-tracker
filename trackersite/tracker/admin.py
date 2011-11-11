c458f7290e4549ced18a480fc8ac39b036a43596
Petr Novak <petr.novak@cosmoboy.cz>
2011-11-11 10:19:16 +0100

    ticket admin: show ID in the list

--- trackersite/tracker/admin.py
+++ trackersite/tracker/admin.py
@@ -17,7 +17,8 @@ class TicketAdmin(admin.ModelAdmin):
             return qs.extra(where=['topic_id in (select topic_id from tracker_topic_admin where user_id = %s)'], params=[request.user.id])
     
     exclude = ('updated', )
-    list_display = ('summary', 'topic', 'requested_by', 'state_str')
+    list_display = ('id', 'summary', 'topic', 'requested_by', 'state_str')
+    list_display_links = ('summary',)
     list_filter = ('topic', 'state')
     inlines = [MediaInfoAdmin, ExpeditureAdmin]
 admin.site.register(models.Ticket, TicketAdmin)
