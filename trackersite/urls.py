406240d7ee7d395fbdfb975faa3b4deb6f7bd4e5
Petr Novak <petr.novak@cosmoboy.cz>
2012-01-20 21:21:44 +0100

    ban robots

--- trackersite/urls.py
+++ trackersite/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, include, url
+from django.http import HttpResponse
 from django.views.generic import RedirectView, TemplateView
 from django.contrib import admin
 from django.contrib.auth import views as auth
@@ -22,4 +23,5 @@ urlpatterns = patterns('',
     url(r'^lang/$', TemplateView.as_view(template_name='choose_language.html'), name='choose_language'),
     url(r'^lang/set/$', 'django.views.i18n.set_language', name='set_language'),
     url(r'^js/i18n\.js$', 'django.views.i18n.javascript_catalog', js_info_dict),
+    url(r'^robots\.txt$', lambda x: HttpResponse("User-agent: *\nDisallow: /\n", mimetype="text/plain")),
 )
