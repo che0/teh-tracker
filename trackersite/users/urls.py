ea072a03ded0c66ad341f535332a4e803d17e938
Petr Novak <petr.novak@cosmoboy.cz>
2015-02-06 21:40:48 +0100

    fix password reset and change url names
    (this broke the features)

--- trackersite/users/urls.py
+++ trackersite/users/urls.py
@@ -7,15 +7,19 @@ urlpatterns = patterns('',
     url(r'^logout/$', auth.logout, kwargs={'template_name':'users/logout.html'}, name='logout'),
     url(r'^register/$', 'users.views.register', name='register'),
     url(r'^password/change/$', auth.password_change, name='password_change', kwargs={'template_name':'users/password_change.html'}),
-    url(r'^password/change/done/$', auth.password_change_done, kwargs={'template_name':'users/password_change_done.html'}),
+    url(r'^password/change/done/$', auth.password_change_done, name='password_change_done', kwargs={
+            'template_name':'users/password_change_done.html',
+        }),
     url(r'^password/reset/$', auth.password_reset, kwargs={
             'template_name':'users/password_reset.html', 'email_template_name':'users/password_reset_email.html'
         }, name='password_reset'),
-    url(r'^password/reset/sent/$', auth.password_reset_done, kwargs={'template_name':'users/password_reset_done.html'}),
-    url(r'^password/reset/(?P<uidb36>[^/]+)/(?P<token>[^/]+)/$', auth.password_reset_confirm, kwargs={
-            'template_name':'users/password_reset_confirm.html'
+    url(r'^password/reset/sent/$', auth.password_reset_done, name='password_reset_done', kwargs={
+            'template_name':'users/password_reset_done.html',
         }),
-    url(r'^password/reset/done/$', auth.password_reset_complete, kwargs={
+    url(r'^password/reset/(?P<uidb36>[^/]+)/(?P<token>[^/]+)/$', auth.password_reset_confirm, name='password_reset_confirm', kwargs={
+            'template_name':'users/password_reset_confirm.html',
+        }),
+    url(r'^password/reset/done/$', auth.password_reset_complete, name='password_reset_complete', kwargs={
             'template_name':'users/password_reset_complete.html'
         }),
 )
