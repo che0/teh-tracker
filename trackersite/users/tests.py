b8b98b62d8fb91d513f603028541fd8331f2d646
Petr Novak <petr.novak@cosmoboy.cz>
2015-06-05 22:18:18 +0200

    fix password reset e-mail

--- trackersite/users/tests.py
+++ trackersite/users/tests.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.core import mail
 from django.test import TestCase
 from django.test.client import Client
 from django.core.urlresolvers import reverse
@@ -24,19 +25,33 @@ class CreateUserTest(TestCase):
 
 class PasswordResetTests(TestCase):
     def test_password_reset(self):
-        response = Client().post(reverse(auth_views.password_reset))
+        response = Client().get(reverse(auth_views.password_reset))
         self.assertEqual(200, response.status_code)
+        
+    def test_password_reset_requested(self):
+        USERNAME, PW, EMAIL = 'foouser', 'foo', 'foo@example.com'
+        u = User.objects.create(username=USERNAME, email=EMAIL)
+        u.set_password(PW)
+        u.save()
+        
+        response = Client().post(reverse(auth_views.password_reset), {
+            'email': EMAIL,
+        })
+        self.assertRedirects(response, reverse(auth_views.password_reset_done))
+        
+        # sent an e-mail
+        self.assertEqual(len(mail.outbox), 1)
     
     def test_password_reset_done(self):
-        response = Client().post(reverse(auth_views.password_reset_done))
+        response = Client().get(reverse(auth_views.password_reset_done))
         self.assertEqual(200, response.status_code)
 
     def test_password_reset_confirm(self):
-        response = Client().post(reverse(auth_views.password_reset_confirm, kwargs={
+        response = Client().get(reverse(auth_views.password_reset_confirm, kwargs={
             'uidb64': 'a', 'token': 'a',
         }))
         self.assertEqual(200, response.status_code)
 
     def test_password_reset_complete(self):
-        response = Client().post(reverse(auth_views.password_reset_complete))
+        response = Client().get(reverse(auth_views.password_reset_complete))
         self.assertEqual(200, response.status_code)
