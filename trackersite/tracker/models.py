446e5251c781ed33fe5290d355fb24e1e1e789e3
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-07-11 14:58:22 +0200

    Add Deposit

--- trackersite/tracker/models.py
+++ trackersite/tracker/models.py
@@ -107,6 +107,7 @@ class Ticket(CachedModel):
     mandatory_report = models.BooleanField(_('report mandatory'), default=False, help_text=_('Is report mandatory?'))
     description = models.TextField(_('description'), blank=True, help_text=_("Space for further notes. If you're entering a trip tell us where did you go and what you did there."))
     supervisor_notes = models.TextField(_('supervisor notes'), blank=True, help_text=_("This space is for notes of project supervisors and accounting staff."))
+    deposit = models.DecimalField(_('deposit'), default=0, help_text=_("If you are requesting a financial deposit, please fill here its amount. Maximum amount is sum of preexpeditures."))
     cluster = models.ForeignKey('Cluster', blank=True, null=True, on_delete=models.SET_NULL)
     payment_status = models.CharField(_('payment status'), max_length=20, default='n/a', choices=PAYMENT_STATUS_CHOICES)
     
