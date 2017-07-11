9b07dfece01ce63be9006efb0793b349885a7494
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-07-11 11:26:44 +0200

    Remove payment_status, use checkbox for future tickets

--- trackersite/tracker/admin.py
+++ trackersite/tracker/admin.py
@@ -36,10 +36,10 @@ class TicketAdmin(admin.ModelAdmin):
         extra_context['add_ack_form'] = AddAckForm()
         return super(TicketAdmin, self).change_view(request, object_id, extra_context=extra_context)
     
-    exclude = ('updated', 'sort_date', 'cluster')
-    readonly_fields = ('state_str', 'payment_status', 'requested_user_details')
-    list_display = ('sort_date', 'id', 'summary', 'topic', 'requested_by', 'state_str', 'payment_status')
-    list_display_links = ('summary',)
+    exclude = ('updated', 'sort_date', 'cluster', 'payment_status')
+    readonly_fields = ('state_str', 'requested_user_details')
+    list_display = ('sort_date', 'id', 'summary', 'topic', 'requested_by', 'state_str')
+    list_display_links = ('summary')
     list_filter = ('topic', 'payment_status')
     date_hierarchy = 'sort_date'
     search_fields = ['id', 'requested_user__username', 'requested_text', 'summary']
