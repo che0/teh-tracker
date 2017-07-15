9d54b392333af5003f7710b599b76ac0b187abf7
Petr Novak <petr.novak@cosmoboy.cz>
2017-07-15 17:59:44 +0200

    travis-ci stuff

---
+++ trackersite/travis_settings.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Django project settings for Travis-CI
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tracker',
+        'USER': 'travis',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'tracker-test-cache',
+    }
+}
+
+SECRET_KEY='42'
+TRACKER_DOCS_ROOT = os.path.join(os.environ['DEPLOY_DIR'], 'docs')
+SITE_ID = 1
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+STATIC_URL = '/static/'
+RECAPTCHA_PUBLIC_KEY = ''
+RECAPTCHA_PRIVATE_KEY = ''
+SENDFILE_BACKEND = 'sendfile.backends.development'
+
+import common_settings as _common
+for item in dir(_common):
+    if item not in _common._IGNORE:
+        locals()[item] = getattr(_common, item)
