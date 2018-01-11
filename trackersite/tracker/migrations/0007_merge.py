53bb217e799aead5d1b82cfeb82ced1464b3a2d4
Martin Urbanec <martin@urbanec.cz>
2018-01-11 06:04:07 +0000

    Create merge migrations due to conflict

---
+++ trackersite/tracker/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tracker', '0006_ticket_imported'),
+        ('tracker', '0003_auto_20180105_0954'),
+    ]
+
+    operations = [
+    ]
