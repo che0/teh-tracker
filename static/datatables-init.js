ba7311f19312fc1caaf73be640a20c8c7a33147f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-05 16:04:47 +0200

    Custom ordering

--- static/datatables-init.js
+++ static/datatables-init.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
 	var language = $('meta[http-equiv="Content-Language"]').attr('content');
-	var fullLanguage = "English";
+    var fullLanguage = "English";
 	switch(language) {
 		case "cs":
 			fullLanguage = "Czech";
 	}
-	var url = "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/" + fullLanguage + ".json";
+    var url = "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/" + fullLanguage + ".json";
+    var ordering = $('#custom-ordering-datatables').text();
+    if(ordering == "")
+    {
+        ordering = [[ 0, "asc "]];
+    }
+    else
+    {
+        ordering = JSON.parse(ordering)
+    }
 	$('table').DataTable({
-		"order": [[ 0, "desc "]],
+		"order": ordering,
 		"language": {
 			"url": url,
 		}
