ad288fd6fc8685db0a397f4128f78d25520e63ce
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-26 19:53:59 +0200

    Fix a typo

--- trackersite/tracker/management/commands/sendnotifications.py
+++ trackersite/tracker/management/commands/sendnotifications.py
@@ -17,7 +17,7 @@ class Command(NoArgsCommand):
             ticket_new_notifs = Notification.objects.filter(target_user=user, notification_type="ticket_new")
             comment_notifs = Notification.objects.filter(target_user=user, notification_type="comment")
 
-            if len(ack_notifs) > 0 or len(ticket_new_notifs) > 0 or len(ticket_change_notifs) > 0 or len(comment_notifs) > 0:
+            if len(ack_notifs) > 0 or len(ticket_new_notifs) > 0 or len(comment_notifs) > 0:
                 c_dict = {u"ack_notifs": ack_notifs, u"ticket_new_notifs": ticket_new_notifs, u"comment_notifs": comment_notifs, u"ABSOLUTE_URL": settings.ABSOLUTE_URL}
                 c = Context(c_dict)
                 user.email_user(subject_text, body_template.render(c), html_message=html_template.render(c))
