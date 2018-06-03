9bb99ad5ab997e0209b5c5d8d91073c9ead562ab
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-03 15:38:11 +0200

    sendnotification.py should not fail when user has no mail

--- trackersite/tracker/management/commands/sendnotifications.py
+++ trackersite/tracker/management/commands/sendnotifications.py
@@ -17,14 +17,15 @@ class Command(NoArgsCommand):
         subject_text = get_template('notification/notification_subject.txt').render(subject_c)
         html_template = get_template('notification/notification_html.html')
         for user in User.objects.all():
-            if len(Notification.objects.filter(target_user=user)) > 0:
-                c_dict = {
-                    "ack_notifs": Notification.objects.filter(target_user=user, notification_type__in=["ack", "ack_remove"]),
-                    "ticket_new_notifs": Notification.objects.filter(target_user=user, notification_type="ticket_new"),
-                    "comment_notifs": Notification.objects.filter(target_user=user, notification_type="comment"),
-                    "supervisor_notes_notifs": Notification.objects.filter(target_user=user, notification_type="supervisor_notes"),
-                }
-                c = Context(c_dict)
-                user.email_user(subject_text, strip_tags(html_template.render(c)), html_message=html_template.render(c))
+            if user.email:
+                if len(Notification.objects.filter(target_user=user)) > 0:
+                    c_dict = {
+                        "ack_notifs": Notification.objects.filter(target_user=user, notification_type__in=["ack", "ack_remove"]),
+                        "ticket_new_notifs": Notification.objects.filter(target_user=user, notification_type="ticket_new"),
+                        "comment_notifs": Notification.objects.filter(target_user=user, notification_type="comment"),
+                        "supervisor_notes_notifs": Notification.objects.filter(target_user=user, notification_type="supervisor_notes"),
+                    }
+                    c = Context(c_dict)
+                    user.email_user(subject_text, strip_tags(html_template.render(c)), html_message=html_template.render(c))
             for notification in Notification.objects.filter(target_user=user):
                 notification.delete()
