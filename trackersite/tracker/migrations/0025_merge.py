c3af6a6cbeedb9d535c2451163a538bb12f99c00
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-14 10:05:00 +0200

    Merge migration

---
+++ trackersite/tracker/migrations/0025_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tracker', '0024_auto_20180614_0815'),
+        ('tracker', '0023_auto_20180612_2024'),
+    ]
+
+    operations = [
+    ]
